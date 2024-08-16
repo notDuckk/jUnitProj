6f7b8691e3b9c7dffc5ff6aa3b360e2c4d41e778
notDuck <daylenfrancis908@gmail.com>
2024-08-15 21:35:04 -0400

    final touches to the testing classes

--- src/test/java/org/example/testBookService.java
+++ src/test/java/org/example/testBookService.java
@@ -1,2 +1,90 @@
-package org.example;public class testBookService {
+package org.example;
+
+import static org.mockito.Mockito.*;
+import org.junit.Before;
+import org.junit.jupiter.api.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import java.util.List;
+
+public class testBookService {
+
+    @Mock
+    BookService bookService = new BookService();
+    Book book1 = new Book("title","author","genre",5.00);
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Before all book service tests");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("After all book service tests");
+    }
+
+    @Test
+    public void testBookSearchPositive(){
+        bookService.addBook(book1);
+        List<Book> result = bookService.searchBook("title");
+        assertEquals(result.get(0).getTitle(),book1.getTitle());
+    }
+
+    @Test
+    public void testBookSearchNegative(){
+        bookService.addBook(book1);
+        List<Book> result = bookService.searchBook("hello");
+        assertTrue(result.isEmpty());
+
+    }
+
+    @Test
+    public void testBookSearchEdge(){
+        bookService.addBook(book1);
+        List<Book> result = bookService.searchBook("");
+
+        assertFalse(result.isEmpty(),"should return all books");
+    }
+
+    @Test
+    public void testPurchaseBookSuccess(){
+        bookService.addBook(book1);
+        User user = new User("test","pass","test@test.com");
+
+        boolean result = bookService.purchaseBook(user,book1);
+
+        assertTrue(result, "purchase should go through if book exists");
+    }
+
+    @Test
+    public void testPurchaseBookfail(){
+        Book book = new Book("title","author","genre",5.00);
+        User user = new User("test","pass","test@test.com");
+        boolean result = bookService.purchaseBook(user,book);
+        assertFalse(result, "purchase should not go through if book doesnt exist");
+    }
+
+
+    @Test
+    public void testPurchaseBookEdge(){
+        //creating a book without a title
+        Book book = new Book("","","",0.00);
+        //adding the book to the book service
+        bookService.addBook(book);
+        //creating a user to purchase the book
+        User user = new User("test","pass","test@test.com");
+
+        //if the books title is an empty string assert false because you cannot purchase a book without a title
+        if (book.getTitle().isEmpty()){
+            assertFalse(false, "cannot purchase a book without a tittle");
+        }else{
+            //else create a variable that returns true if the user and book exist and has a title
+            boolean result = bookService.purchaseBook(user,book);
+            assertTrue(result, "the purchase was succesful");
+        }
+    }
+
 }
--- src/test/java/org/example/testUserService.java
+++ src/test/java/org/example/testUserService.java
@@ -1,2 +1,80 @@
-package org.example;public class testUserServive {
+package org.example;
+
+import org.example.UserService;
+import org.example.User;
+
+import static org.mockito.Mockito.*;
+import org.junit.Before;
+import org.junit.jupiter.api.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.MockitoAnnotations;
+
+
+@DisplayName("Testing for user service class")
+public class testUserService {
+
+    @Mock
+    UserService userService = new UserService();
+    User newUser = new User("notDuck","password","test@test.com");
+    User newUser2 = new User(null,null,null);
+    //test to see if edge case is working
+//    User newUser = new User("","","");
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Before all user service tests");
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After all user service tests");
+    }
+
+
+    public void setUp(){
+
+    }
+
+    @Test
+    void testUserRegisterTrue(){
+        assertTrue(userService.registerUser(newUser));
+    }
+
+    @Test
+    void testUserRegisterFalse(){
+        assertFalse(!userService.registerUser(newUser));
+    }
+
+    @Test
+    void testUserRegisterEdge(){
+        if (newUser.getUsername().isEmpty() || newUser.getPassword().isEmpty() || newUser.getEmail().isEmpty()) {
+            assertFalse(userService.registerUser(newUser));
+        }else{
+            assertTrue(userService.registerUser(newUser));
+        }
+    }
+
+    @Test
+    void testUserLoginTrue(){
+        assertNotNull(newUser.getUsername(),newUser.getPassword());
+        userService.loginUser(newUser.getUsername(),newUser.getPassword());
+    }
+
+    @Test
+    void testUserLoginFalse(){
+        assertNull(newUser2.getUsername());
+    }
+
+    @Test
+    void testUserLoginEdge(){
+        if(newUser2.getUsername()==null || newUser2.getPassword()==null) {
+            assertNull(newUser2.getUsername(),newUser2.getPassword());
+        }else {
+            userService.loginUser(newUser2.getUsername(),newUser2.getPassword());
+        }
+    }
+
 }
